2b22fd073a281f62acb175d4eea300d861dfdbf3
Ubaid Raza <154611273+ubaiidraza@users.noreply.github.com>
2025-01-31 16:35:02 +0500

    kuch ha

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", "./src/**/*.{js,jsx,ts,tsx}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light"],
+  },
+  
+};;
